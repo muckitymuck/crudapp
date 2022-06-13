e418d07901f069c2bbd2553efdd2b3267fe48c92
andre <andrewgoldstein@hotmail.com>
2022-06-13 15:15:25 -0500

    update

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ require('dotenv').config()
 
 
 let db, 
-    dbConnectionStr = process.env.DB_STRING
+    dbConnectionStr = process.env.DB_STRING,
     dbName = 'star-trek-api'
 
 MongoClient.connect(dbConnectionStr)
     .then(client => {
         console.log(`Connected to Database`)
         db = client.db(dbName)
-
     })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
