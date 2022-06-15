5ba422ad7da97241ae52800242fcbf4cfa87b92c
muckity <andrewgoldstein@hotmail.com>
2022-06-14 20:11:46 -0500

    final- looking good

--- server.js
+++ server.js
@@ -21,19 +21,64 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-
+//CRUD Methods
 app.get('/', (request,response) => {
     let contents = db.collection('alien-info').find().toArray()
-
-    console.log(contents)
+    .then(data => {
+        let namelist = data.map(item => item.speciesName)
+        console.log(namelist)
+        response.render('index.ejs', {info: namelist})
+    })
+    .catch(error => console.log(error))
 })
 
 app.post('/api', (request,response) => {
 
+    console.log("post heard")
+    db.collection('alien-info').insertOne(
+        request.body
+    )
+    .then(result => {
+        console.log(result)
+        response.redirect('/')
+    })
+
+
 })
 
 app.put('/updateEntry', (request,response) => {
-    
+    console.log(request.body)
+    Object.keys(request.body).forEach(key => {
+        if(request.body[key] === null || request.body[key] === undefined || request.body[key] === "") {
+            delete request.body[key]
+        }        
+    });
+    console.log(request.body)
+    db.collection('alien-info').findOneAndUpdate(
+        {name: request.body.name},
+        {
+            $set: request.body
+        }
+
+    )
+    .then(result => {
+        console.log(result)
+        console.log('Success')
+
+    })
+    .catch(error => console.error(error))
+})
+
+app.delete('/deleteEntry', (request,response) => {
+    db.collection('alien-info').deleteOne(
+        {name: request.body.name}
+    )
+    .then(result => {
+        console.log('Entry Deleted')
+        response.json('Entry Deleted')
+    })
+    .catch(error => console.error(error))
+
 })
 
 
